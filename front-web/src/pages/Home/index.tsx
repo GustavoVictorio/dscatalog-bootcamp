715ed78deb15e62a6d64ff1d3aa1cb134def4641
Gustavo Victorio <gu.victorio3@gmail.com>
2020-10-08 09:19:52 -0300

    Finish product listing layout

--- front-web/src/pages/Home/index.tsx
+++ front-web/src/pages/Home/index.tsx
@@ -4,17 +4,16 @@ import ButtonIcon from '../../core/components/Buttonicon';
 import { Link } from 'react-router-dom';
 import './styles.scss'
 
-
 const Home = () => (
   <div className="home-container">
-    <div className="row home-content">
-      <div className="col-6">
+    <div className="row home-content card-base border-radius-20">
+      <div className="col-6 home-text">
         <h1 className="text-title">
           Conheça o melhor <br /> catálogo de produtos
           </h1>
-        <p className="text-subtitle">
-          Ajudaremos você a encontrar os melhores <br />  produtos disponíveis no mercado.
-      </p>
+          <p className="text-subtitle">
+            Ajudaremos você a encontrar os melhores <br />  produtos disponíveis no mercado.
+          </p>
         <Link to="/catalog">
           <ButtonIcon text="inicie agora a sua busca" />
         </Link>
